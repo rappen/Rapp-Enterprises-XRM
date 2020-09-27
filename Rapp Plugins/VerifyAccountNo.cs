77a8f91ab27a00c9e70b99f9e9082438b7d0c41e
Jonas Rapp <jonas@jonasr.app>
2020-09-27 22:55:14 +0200

    Add project files.

---
+++ Rapp Plugins/VerifyAccountNo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Rapp_Plugins
+{
+    public class VerifyAccountNo : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = new Lazy<IOrganizationService>(() => factory.CreateOrganizationService(context.UserId));
+
+            if (context.InputParameters.ContainsKey("Target") && context.InputParameters["Target"] is Entity target)
+            {
+                if (!target.Contains("accountnumber"))
+                {   // All good
+                    return;
+                }
+                var accountnumber = target["accountnumber"] as string;
+
+            }
+        }
+    }
+}
