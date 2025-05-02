7a5fcf074a9c81e20e36f9cbcff7a29cd0232f6b
Jonas Rapp <jonas@jonasr.app>
2025-05-02 12:54:59 +0200

    Tiny nicer code

--- Rapp Plugins/ListContactsFirstNames.cs
+++ Rapp Plugins/ListContactsFirstNames.cs
@@ -8,12 +8,13 @@ namespace Rapp_Plugins
 {
     public class ListContactsFirstNames : IPlugin
     {
-        /* 
-         * Concatenate first names of all Contacts to 
+        /*
+         * Concatenate first names of all Contacts to
          * the Description field of their parent Account.
-         * 
+         *
          * Triggered on Create and Update of Contact.
          */
+
         public void Execute(IServiceProvider serviceProvider)
         {
             var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
@@ -31,50 +32,56 @@ namespace Rapp_Plugins
                 tracer.Trace($"Wrong entity: {context.PrimaryEntityName}");
                 return;
             }
-
-            if (context.InputParameters.ContainsKey("Target") && context.InputParameters["Target"] is Entity target && target.Contains("parentcustomerid"))
+            if (!context.InputParameters.ContainsKey("Target") || !(context.InputParameters["Target"] is Entity target) || !target.Contains("parentcustomerid"))
             {
-                var accountref = target["parentcustomerid"] as EntityReference;
-                if (accountref.Id.Equals(Guid.Empty))
-                {
-                    if (context.PreEntityImages.Count > 0 && context.PreEntityImages[context.PreEntityImages.Keys.First()] is Entity preimage && preimage.Contains("parentcustomerid"))
-                    {
-                        accountref = preimage["parentcustomerid"] as EntityReference;
-                    }
-                }
-                if (accountref.Id.Equals(Guid.Empty))
-                {
-                    return;
-                }
+                tracer.Trace("Target is not an entity or does not contain parentcustomerid.");
+                return;
+            }
 
-                var sw = Stopwatch.StartNew();
-                var account = service.Retrieve("account", accountref.Id, new ColumnSet("accountid", "name", "description"));
-                sw.Stop();
-                tracer.Trace($"Retrieved account: {account["name"]} in {sw.ElapsedMilliseconds} ms");
+            var accountref = target["parentcustomerid"] as EntityReference;
+            if ((accountref == null || accountref.Id.Equals(Guid.Empty)) &&
+                context.PreEntityImages.Count > 0 &&
+                context.PreEntityImages[context.PreEntityImages.Keys.First()] is Entity preimage &&
+                preimage.Contains("parentcustomerid"))
+            {
+                accountref = preimage["parentcustomerid"] as EntityReference;
+            }
+            if (accountref.Id.Equals(Guid.Empty))
+            {
+                tracer.Trace("No parentcustomerid, just exit.");
+                return;
+            }
 
-                account.TryGetAttributeValue("description", out string contactlist);
-                
-                var query = new QueryExpression("contact");
-                query.ColumnSet = new ColumnSet("firstname");
-                query.Criteria.AddCondition("parentcustomerid", ConditionOperator.Equal, accountref.Id);
-                query.AddOrder("firstname", OrderType.Ascending);
+            var sw = Stopwatch.StartNew();
+            var account = service.Retrieve("account", accountref.Id, new ColumnSet("accountid", "name", "description"));
+            sw.Stop();
+            tracer.Trace($"Retrieved account: {account["name"]} in {sw.ElapsedMilliseconds} ms");
 
+            account.TryGetAttributeValue("description", out string contactlist);
+
+            var query = new QueryExpression("contact");
+            query.ColumnSet = new ColumnSet("firstname");
+            query.Criteria.AddCondition("parentcustomerid", ConditionOperator.Equal, accountref.Id);
+            query.AddOrder("firstname", OrderType.Ascending);
+
+            sw.Restart();
+            var contacts = service.RetrieveMultiple(query);
+            sw.Stop();
+            tracer.Trace($"Retrieved {contacts.Entities.Count} contacts in {sw.ElapsedMilliseconds} ms");
+
+            var newcontactlist = contacts.Entities
+                .Where(c => c.Contains("firstname"))
+                .Select(c => c["firstname"] as string)
+                .Distinct();
+
+            if (!newcontactlist.Equals(contactlist))
+            {
+                var updaccount = new Entity("account", account.Id);
+                updaccount["description"] = newcontactlist;
                 sw.Restart();
-                var contacts = service.RetrieveMultiple(query);
+                service.Update(updaccount);
                 sw.Stop();
-                tracer.Trace($"Retrieved {contacts.Entities.Count} contacts in {sw.ElapsedMilliseconds} ms");
-
-                var newcontactlist = contacts.Entities
-                    .Where(c => c.Contains("firstname"))
-                    .Select(c => c["firstname"] as string)
-                    .Distinct();
-
-                if (!newcontactlist.Equals(contactlist))
-                {
-                    var updaccount = new Entity("account", account.Id);
-                    updaccount["description"] = newcontactlist;
-                    service.Update(updaccount);
-                }
+                tracer.Trace($"Updated account: {account["name"]} with {newcontactlist.Count()} contacts in {sw.ElapsedMilliseconds} ms");
             }
         }
     }
--- Rapp Plugins/VerifyAccountNo.cs
+++ Rapp Plugins/VerifyAccountNo.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xrm.Sdk;
 using System;
+using System.Linq;
 
 namespace Rapp_Plugins
 {
@@ -7,9 +8,10 @@ namespace Rapp_Plugins
     {
         /*
          * Verify that Account Number is numeric
-         * 
+         *
          * Triggered on Create of Account.
          */
+
         public void Execute(IServiceProvider serviceProvider)
         {
             var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
@@ -22,25 +24,35 @@ namespace Rapp_Plugins
                 tracer.Trace($"Wrong message: {context.MessageName}");
                 return;
             }
+            if (context.Stage != 10)
+            {
+                tracer.Trace($"Wrong stage: {context.Stage}");
+                return;
+            }
             if (context.PrimaryEntityName != "account")
             {
                 tracer.Trace($"Wrong entity: {context.PrimaryEntityName}");
                 return;
             }
-
-            if (context.InputParameters.ContainsKey("Target") && context.InputParameters["Target"] is Entity target)
+            if (!context.InputParameters.ContainsKey("Target") || !(context.InputParameters["Target"] is Entity target))
             {
-                if (!target.Contains("accountnumber"))
-                {   // All good
-                    return;
-                }
-                var accountnumber = target["accountnumber"] as string;
-
-                if (!int.TryParse(accountnumber, out int numericnumber))
-                {
-                    throw new InvalidPluginExecutionException("Account Number must be numeric.");
-                }
+                tracer.Trace("Target is not an entity.");
+                return;
             }
+            if (!target.Contains("accountnumber"))
+            {   // All good
+                tracer.Trace("Account Number not set.");
+                return;
+            }
+
+            var accountnumber = target["accountnumber"] as string;
+
+            if (accountnumber.Any(c => !char.IsDigit(c)))
+            {
+                throw new InvalidPluginExecutionException($"Account Number must be numeric. ({accountnumber})");
+            }
+
+            tracer.Trace($"Account Number is numeric: {accountnumber}");
         }
     }
 }
